b47b2d6522980e10170982bf7a77745281328907
ArmiNigere <armstrongnigere@hotmail.com>
2017-01-21 16:15:09 +0100

    registartion

--- src/watchNeihbours/Registrazione.java
+++ src/watchNeihbours/Registrazione.java
@@ -2,4 +2,19 @@ package watchNeihbours;
 
 public class Registrazione {
 
+
+
+	private Utente user;
+	private Citta city;
+	private Quartier abitazione; //abitazione sta per quartiere
+	
+	public Registrazione(Utente user, Citta city, Quartier abitazione) {
+		super();
+		user = user;
+		this.city = city;
+		this.abitazione = abitazione;
+	}
+
+
+
 }
